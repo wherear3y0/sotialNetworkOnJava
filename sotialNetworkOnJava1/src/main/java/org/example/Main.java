89fe24ac638a4a4ff1dc94b69e812190ce686cd9
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2022-12-21 20:14:30 +0300

    я понял что parser не нужен
    По задаче можно делать и без интерфейса - так что задача облегчается

--- sotialNetworkOnJava1/src/main/java/org/example/Main.java
+++ sotialNetworkOnJava1/src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
 
-import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
-        JsonParser<Root> jsonParser = new JsonParser<>();
-        Root root = jsonParser.parse(Root.class);
-        System.out.println(root.toString());
+
     }
 
 
--- sotialNetworkOnJava1/src/main/java/org/example/gsonParser.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class gsonParser {
-
-    public Root parse(){
-        return null;
-    }
-}
