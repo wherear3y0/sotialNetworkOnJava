d11fde657a70b47b2df9c0f647c27a3d207fa4bd
whereareyo <104694707+wherear3y0@users.noreply.github.com>
2022-12-23 15:52:12 +0300

    добавил в main остальные сервисы

--- sotialNetworkOnJava1/src/main/java/org/example/PersonController.java
+++ sotialNetworkOnJava1/src/main/java/org/example/PersonController.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class PersonController {
     public static void main(String[] args) {
+        
         PersonService personService = new PersonService();
+        FriendService friendService = new FriendService();
+        LikeService likeService = new LikeService();
+        PhotoService photoService = new PhotoService();
+
         Scanner scanner= new Scanner(System.in);
+
         while (true) {
             try {
                 var line = scanner.nextLine().split(" "); // addd 20 name name
                 switch (line[0]) {
-                    case "11111" -> System.out.println();
-                    case "addd" -> personService.addPerson(Integer.parseInt(line[1]), line[2], Integer.parseInt(line[3]));
-
+                    case "1" -> System.out.println();
+                    case "add" -> personService.addPerson(Integer.parseInt(line[1]), line[2],
+                                                                    Integer.parseInt(line[3]));
                     default -> System.out.println("Not found actio");
                 }
             } catch (RuntimeException e) {
